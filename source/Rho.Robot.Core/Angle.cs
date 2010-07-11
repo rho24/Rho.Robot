22d0b08b706f195b93c4633b666dc1381f213546
Richard Oliver <richard@rholiver.co.uk>
2010-07-11 17:45:15 +0100

    Setup project files

---
+++ source/Rho.Robot.Core/Angle.cs
@@ -0,0 +1,86 @@
+using System;
+
+namespace Rho.Robot.Core
+{
+    public struct Angle
+    {
+        const double TWO_PI =  Math.PI * 2.0;
+        const double RAD_TO_DEG = 360 / (2 * Math.PI);
+        const double DEG_TO_RAD = (2 * Math.PI) / 360;
+
+        private double _RawRadians;
+        public double RawRadians
+        {
+            get
+            {
+                return _RawRadians;
+            }
+        }
+
+        public double RawDegrees
+        {
+            get
+            {
+                return RawRadians * RAD_TO_DEG;
+            }
+        }
+
+        private double _Radians;
+        public double Radians
+        {
+            get
+            {
+                return _Radians;
+            }
+        }
+
+        public double Degrees
+        {
+            get
+            {
+                return Radians * RAD_TO_DEG;
+            }
+        }
+
+        private Angle(double radians)
+        {
+            _RawRadians = radians;
+            _Radians = (((((radians % TWO_PI) + TWO_PI) % TWO_PI) + Math.PI) % TWO_PI) - Math.PI;
+        }
+
+        public static Angle FromRadians(double radians)
+        {
+            return new Angle(radians);
+        }
+
+        public static Angle FromDegrees(double degrees)
+        {
+            return new Angle(degrees * DEG_TO_RAD);
+        }
+
+        public static Angle Zero
+        {
+            get { return new Angle(); }
+        }
+
+        public static Angle operator +(Angle a1, Angle a2)
+        {
+            return new Angle(a1.RawRadians + a2.RawRadians);
+        }
+
+        public static Angle operator -(Angle a1, Angle a2)
+        {
+            return new Angle(a1.RawRadians - a2.RawRadians);
+        }
+
+        public static Angle operator *(Angle a, double d)
+        {
+            return new Angle(a.RawRadians * d);
+        }
+
+        public static Angle operator /(Angle a, double d)
+        {
+            return new Angle(a.RawRadians / d);
+        }
+    }
+}
---
+++ source/Tests/Rho.Robot.Core.Tests/Angle_Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Rho.Robot.Core;
+
+namespace Rho.Robot.Core.Tests
+{
+    public class Angle_Tests
+    {
+        [Fact]
+        public void Default_Should_Be_Zero()
+        {
+            Angle ang = default(Angle);
+
+            Assert.Equal(0.0, ang.Degrees);
+            Assert.Equal(0.0, ang.Radians);
+            Assert.Equal(0.0, ang.RawDegrees);
+            Assert.Equal(0.0, ang.RawRadians);
+        }
+    }
+}
